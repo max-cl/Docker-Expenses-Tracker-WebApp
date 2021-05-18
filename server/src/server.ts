132b775498bdcc397410987136ca0ed11afa7b1b
Max <maximiliano.arias@gmail.com>
2021-05-18 22:36:46 +0200

    #

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,17 @@
-import express, { Application, Request, Response, NextFunction } from 'express';
-import cors from 'cors';
-import helmet from 'helmet';
-import passport from 'passport';
-import compression from 'compression';
-import 'config/passport.config';
-import { errorHandler } from 'middlewares/error.middleware';
-import { notFoundHandler } from 'middlewares/notFound.middleware';
+import express, { Application, Request, Response, NextFunction } from "express";
+import cors from "cors";
+import helmet from "helmet";
+import passport from "passport";
+import compression from "compression";
+import "config/passport.config";
+import { errorHandler } from "middlewares/error.middleware";
+import { notFoundHandler } from "middlewares/notFound.middleware";
 
 // Routes
-import router from 'routes/';
+import router from "routes/";
 
 // Utils
-import { checkDbConnection, syncForce, syncPasive, syncAlter } from 'utils/sequelize.util';
+import { checkDbConnection, syncForce, syncPasive, syncAlter } from "utils/sequelize.util";
 
 class Server {
     public app: Application;
@@ -33,8 +33,8 @@ class Server {
         this.app.use(helmet());
         this.app.use(passport.initialize());
         this.app.use(router);
-        this.app.get('/', (req: Request, res: Response, next: NextFunction) => {
-            res.status(200).json({ message: 'success' });
+        this.app.get("/", (req: Request, res: Response, next: NextFunction) => {
+            res.status(200).json({ message: "success" });
         });
 
         this.app.use(errorHandler);
